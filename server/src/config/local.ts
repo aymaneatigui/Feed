08fba475879208aa3b5dca95b9564b1e52de63f4
aymane <atigui.cs@gmail.com>
2024-02-01 14:45:22 +0100

    create environment configurations

---
+++ server/src/config/local.ts
@@ -0,0 +1,5 @@
+export default {
+  port: 3001,
+  DATABASE_URL:
+    "mongodb+srv://manager:JoO2tWOW0GD3hlRV@cluster0.wsno5to.mongodb.net/feed?retryWrites=true&w=majority",
+};
---
+++ server/src/config/prod.ts
@@ -0,0 +1,4 @@
+export default {
+  PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
